b8d6afcd6e7df600ed3831d5514c7300042c891f
intannrfrzh <129675607+intannrfrzh@users.noreply.github.com>
2024-05-29 13:13:46 +0800

    1st

--- PUPP-main/public/app/router.php
+++
@@ -1,8 +0,0 @@
-<?php
-$path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-
-if (file_exists(__DIR__ . $path)) {
-    return false; // serve the requested resource as-is
-} else {
-    include_once 'index.php';
-}
---
+++ app/Controller/HomeController.php
@@ -0,0 +1,13 @@
+<?php
+
+class HomeController
+{
+    public function index()
+    {
+        // This method handles requests to the root URL
+        // You can implement logic to render the home page here
+        echo 'Welcome to the home page!';
+    }
+}
+
+?>
