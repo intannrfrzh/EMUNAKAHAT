ac24212c415d989568e1948019948f4890335123
kaekzha <cb21075@student.ump.edu.my>
2024-06-07 15:34:06 +0800

    Update database.php

--- app/Config/database.php
+++ app/Config/database.php
@@ -1,4 +1,3 @@
-
 <?php
 
 $db_server = "localhost";
@@ -6,35 +5,33 @@ $db_user = "root";
 $db_pass = "";
 $db_name = "systemkahwin";
 
-$connect = mysqli_connect($db_server,$db_user,$db_pass,$db_name);
+// Create a MySQLi connection
+$connect = mysqli_connect($db_server, $db_user, $db_pass, $db_name);
 
+if (!$connect) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+;
 
 class Database {
 
-    //Decluring Variable of Server
-    // private $host = "sql12.freemysqlhosting.net";
-    // private $db_name = "sql12617882";
-    // private $username = "sql12617882";
-    // private $password = "Px4zDGzKcL";
-
+    // Declaring Variables of Server
     private $host = "localhost";
     private $db_name = "systemkahwin";
     private $username = "root"; 
     private $password = "";
 
-    
+    // PDO connection method
     public function connect() {
         $conn = null;
         try {
             $conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
             $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            
         } catch (PDOException $exception) {
-
-            //Error Message
+            // Error Message
             echo "Connection error: " . $exception->getMessage();
         }
         return $conn;
     }
 }
-?>
+
